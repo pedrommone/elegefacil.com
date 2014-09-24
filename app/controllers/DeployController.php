ea5fd545119997269e738252d65366f880f744a4
Pedro Maia <pedromaia@dialhost.com.br>
2014-09-24 10:01:50 -0300

    Added http return on deploy route

--- app/controllers/DeployController.php
+++ app/controllers/DeployController.php
@@ -8,8 +8,19 @@ class DeployController extends BaseController {
 			Artisan::call('deploy:make');
 
 			Log::info('Deployment procedure done.');
+
+			Reponse::json([
+				'status' => 'ok',
+				'message' => 'Everything went well.'
+			], 200);
+
 		} catch (Exception $e) {
 			Log::error('Hey, something went very wrong with deployment.');
+
+			Reponse::json([
+				'status' => 'error',
+				'message' => 'Something went very wrong.'
+			], 500);
 		}
 		
 	}
