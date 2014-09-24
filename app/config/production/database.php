d38086b58c3020b5d646280ae7cad480ebd5e0a5
Pedro Maia <pedromaia@dialhost.com.br>
2014-09-24 09:42:13 -0300

    Added some config files

---
+++ app/config/production/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'elegefacil_main',
+			'username'  => 'elegefacil_main',
+			'password'  => 'pedropedro',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		)
+
+	)
+
+);
