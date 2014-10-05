4aeead73b175666f40bf6d8f95a5bdf80b5d2937
Pedro Maia <pedro@pedromm.com>
2014-10-05 14:19:22 -0300

    Added users table

---
+++ app/database/migrations/2014_10_05_171535_add_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table) {
+			$table->increments('id');
+			$table->string('username', 50);
+			$table->string('password', 200);
+
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
