13ccc5cfcf9582f408c2bab9dd35692ebe17c3f6
Pedro Maia <pedro@pedromm.com>
2014-10-20 15:17:45 -0200

    Added schema for Voters

--- app/controllers/Admin/VotersController.php
+++ app/controllers/Admin/VotersController.php
@@ -1,8 +1,52 @@
 <?php namespace Admin;
 
-use \BaseController as BaseController;
-use \View as View;
+use BaseController;
 
 class VotersController extends BaseController {
+
+	protected $model = 'Voter';
+	protected $title = 'eleitores';
+	protected $uri = 'voters';
+
+	protected $properties = [
+		'id' => [
+			'name' => 'Código',
+			'type' => 'primary_key',
+			'casting' => 'integer',
+			'show_list' => true,
+		],
+		'section_id' => [
+			'name' => 'Sessão eleitoral',
+			'type' => 'relationship',
+			'casting' => 'integer',
+			'validation' => 'required|integer',
+			'show_list' => true,
+			'model' => 'Section',
+			'model_desc' => 'id'
+		],
+		'name' => [
+			'name' => 'Nome',
+			'type' => 'text',
+			'casting' => 'string',
+			'validation' => 'required|max:100',
+			'show_list' => true
+		],
+		'gender' => [
+			'name' => 'Sexualidade',
+			'type' => 'select',
+			'casting' => 'string',
+			'validation' => 'required',
+			'show_list' => true,
+			'options' => ['M' => 'Masculino', 'F' => 'Feminino']
+		],
+		'birthday' => [
+			'name' => 'Data de nascimento',
+			'type' => 'text',
+			'casting' => 'string',
+			'validation' => 'required|min:8|max:8',
+			'placeholder' => 'dd/mm/aaaa',
+			'show_list' => true
+		]
+	];
 
 }
