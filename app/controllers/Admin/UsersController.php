68dbffdda777b4ad724532a0569ea0b15e88ed15
Pedro Maia <pedro@pedromm.com>
2014-10-20 15:17:21 -0200

    Added schema for Users

--- app/controllers/Admin/UsersController.php
+++ app/controllers/Admin/UsersController.php
@@ -1,8 +1,36 @@
 <?php namespace Admin;
 
-use \BaseController as BaseController;
-use \View as View;
+use BaseController;
 
 class UsersController extends BaseController {
+
+	protected $model = 'User';
+	protected $title = 'administradores';
+	protected $uri = 'users';
+
+	protected $properties = [
+		'id' => [
+			'name' => 'Código',
+			'type' => 'primary_key',
+			'casting' => 'integer',
+			'validation' => null,
+			'show_list' => true
+		],
+		'username' => [
+			'name' => 'Usuário',
+			'type' => 'text',
+			'casting' => 'string',
+			'validation' => 'required|max:50',
+			'show_list' => true
+		],
+		'password' => [
+			'name' => 'Senha',
+			'type' => 'password',
+			'casting' => 'string',
+			'validation' => 'required|max:30',
+			'show_list' => false,
+			'hide_view' => true
+		]
+	];
 
 }
