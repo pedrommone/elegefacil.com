aaea87e47584e0ee5d050bac4dcb62c493b28137
Pedro Maia <pedro@pedromm.com>
2014-10-20 15:15:59 -0200

    Schema for Sections

--- app/controllers/Admin/SectionsController.php
+++ app/controllers/Admin/SectionsController.php
@@ -1,9 +1,36 @@
 <?php namespace Admin;
 
-use \BaseController as BaseController;
-use \View as View;
+use BaseController;
 
 class SectionsController extends BaseController {
 
+	protected $model = 'Section';
+	protected $title = 'seções eleitorais';
+	protected $uri = 'sections';
+
+	protected $properties = [
+		'id' => [
+			'name' => 'Código',
+			'type' => 'primary_key',
+			'casting' => 'integer',
+			'show_list' => true,
+		],
+		'zone_id' => [
+			'name' => 'Zona eleitoral',
+			'type' => 'relationship',
+			'casting' => 'integer',
+			'validation' => 'required|integer',
+			'show_list' => true,
+			'model' => 'Zone',
+			'model_desc' => 'id'
+		],
+		'address' => [
+			'name' => 'Endereço',
+			'type' => 'text',
+			'casting' => 'string',
+			'validation' => 'required|max:100',
+			'show_list' => true
+		]
+	];
 
 }
